b604ed5294bc73d9bea12c90b53a1056acb1a157
Dandex200-dex <57302290+Dandex200-dex@users.noreply.github.com>
2022-12-03 12:54:40 +0100

    Delete Dreams directory

--- Dreams/testing.js
+++
@@ -1,6 +0,0 @@
-let btn = document.querySelector('#bgchanger');
-let colors = ['mediumseagreen', 'orange', 'gray', 'navy', 'crimson', 'teal'];
-btn.addEventListener('click', ()=> {
-let randomizedColors = colors[Math.floor(Math.random() * colors.length)];
-document.body.style.backgroundColor = randomizedColors;
-});
